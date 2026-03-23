4b44df1a2e7a199903d19ec12b0ece6636c7bd44
João Gabriel De Jesus Cunha <141980608+jogabrieldev@users.noreply.github.com>
2026-03-23 19:05:24 -0300

    update
    Foi montado um refactor nas paginas com alguns detalhes visualizados após uns testes. Adicionado um voltar no header da pagina de busca para reiniciar o processo de busca com mais facilidade. Ajustado detalhes nos ícones da pagina home para melhor visualização, o componente foi melhorado a responsividade do select.

--- src/services/cep.weather.service.ts
+++ src/services/cep.weather.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, switchMap, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/enviroment';
 
 @Injectable({ providedIn: 'root' })
 export class CepWeatherService {
